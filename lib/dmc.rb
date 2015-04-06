58f205cc03a48963e5c9ec52acea83da673a5cb5
Mhairi Parr <m.s.parr.09@aberdeen.ac.uk>
2015-04-06 22:48:06 +0000

    nokogiri setup

--- lib/dmc.rb
+++ lib/dmc.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-   
-PAGE_URL = "http://www.imsdb.com/Movie%20Scripts/How%20to%20Train%20Your%20Dragon%202%20Script.html"
 
-page = Nokogiri::HTML(open(PAGE_URL))
-   
-puts page.css("title")[0].name   # => title
-puts page.css("title")[0].text   # => My webpage
+  PAGE_URL = 'http://www.imsdb.com/scripts/Authors-Anonymous.html'
+  doc = Nokogiri::HTML(open(PAGE_URL))
+  
+  option = doc.css('b').remove
+  option = doc.css('pre')
+ 
+   puts option
